e12b750f39f0c94a7a53d97636677816cb4062ad
Douglas Urner <dlu@canishe.com>
2020-01-15 14:40:15 -0800

    Pull in code for source method.

--- lib/secrets.rb
+++ lib/secrets.rb
@@ -1,6 +1,19 @@
 require "secrets/version"
 
-module Secrets
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module Secrets
+#   class Error < StandardError; end
+  def source(filename)
+    # Inspired by user takeccho at http://stackoverflow.com/a/26381374/3849157
+    # Sources sh-script or env file and imports resulting environment
+    fail(ArgumentError, "File #{filename} invalid or doesn't exist.") \
+       unless File.exist?(filename)
+
+    _env_hash_str = `env -i sh -c 'set -a;source #{filename} && ruby -e "p ENV"'`
+    fail(ArgumentError,"Failure to parse or process #{filename} environment") \
+       unless _env_hash_str.match(/^\{("[^"]+"=>".*?",\s*)*("[^"]+"=>".*?")\}$/)
+
+    _env_hash = eval(_env_hash_str)
+     %w[ SHLVL PWD _ ].each{ |k| _env_hash.delete(k) }
+    _env_hash.each{ |k,v| ENV[k] = v }
+  end
+#end
